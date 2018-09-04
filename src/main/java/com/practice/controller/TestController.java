9e29c4b03e5aa2d3d0aae957ed1b3a5811314164
123 <1107540794@qq.com>
2018-09-04 10:48:35 +0800

    first commit

---
+++ src/main/java/com/practice/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.practice.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @description: <文件描述>
+ * @modified: <文件修改说明>
+ * @auther: ZhuoZhiling
+ * @date: 2018/9/4 9:41
+ * @version: 2.0.0
+ */
+public class TestController extends HttpServlet{
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+        String vegetable = request.getParameter("vegetable");
+        if (vegetable == null) {
+            vegetable = "Tomato";
+        }
+
+        writer.println("<html><body>");
+        writer.println("<h1> Q:What do you like best? </h1>");
+        writer.println("<br>");
+        writer.println("<h1> A:" + vegetable + "</h1>");
+        writer.println("</body></html>");
+    }
+}
